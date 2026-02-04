7a86b06b1e5f9531b1abb73ea89c9422ec159d26
Subham Sarkar <poltu2108@gmail.com>
2026-02-05 00:41:10 +0530

    logo fix

--- index.tsx
+++ index.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// The favicon is now set directly in index.html
-// const favicon = document.querySelector<HTMLLinkElement>('link[rel="icon"]');
-// if (favicon) {
-//   favicon.href = '/logo-new.jpg';
-// }
-
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
